f129ac710626a326e8b2441aaa920c465e1f3dab
Mauricekinyua <maurice.k.muchiri@gmail.com>
2025-07-24 13:14:33 +0300

    HeroSection

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -15,17 +15,18 @@ export default function Home() {
 
     <Navbar/>
     <section className="main-section">
-      <div className="page-heading">
+      <div className="page-heading py-15">
         <h1>Track Your Applications & Resume Ratings</h1>
         <h2>Review your submissions and check AI-Powered feedback</h2>
       </div>
-      </section>
-      {resumes.length>0 &&(
-        <div className="resumes-section">
-          {resumes.map((resume)=>(
-            <ResumeCard key={resumes.id} resume={resume}/>
-          ))}
-        </div>
+      {resumes.length > 0 &&(
+          <div className="resumes-section">
+            {resumes.map((resume)=>(
+                <ResumeCard key={resume.id} resume={resume}/>
+            ))}
+          </div>
       )}
+      </section>
+
   </main>
 }
