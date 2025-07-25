12523089cca0fab78689b4357eebe02dbf93c1ce
Mauricekinyua <maurice.k.muchiri@gmail.com>
2025-07-25 12:49:05 +0300

    auth

---
+++ app/routes/Auth.tsx
@@ -0,0 +1,50 @@
+import {usePuterStore} from "~/lib/puter";
+import {useEffect} from "react";
+import {useLocation} from "react-router";
+import { useNavigate } from "react-router";
+export const meta = () => ([
+    {title: 'Resumai | Auth'},
+    {name: 'description',content: 'Log into Your account'}
+])
+
+const Auth =()=>{
+    const {isLoading, auth} = usePuterStore();
+    const location = useLocation();
+    const next = location.search.split('next')[1];
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+        if(auth.isAuthenticated) navigate(next)
+    },[auth.isAuthenticated, next])
+
+    return(
+        <main className = "bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
+            <div className="gradient-border shadow-lg">
+                <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
+                    <div className="flex flex-col items-center gap-2 text-center">
+                        <h1>Welcome</h1>
+                        <h2>Log In to Continue your Job Journey</h2>
+                    </div>
+                    {isLoading ?(
+                        <button className="auth-button animate-pulse">
+                            <p>Signing you in ...</p>
+                        </button>
+                    ):(
+                        <>
+                            {auth.isAuthenticated ? (
+                                <button className="auth-button" onClick={auth.signOut}>
+                                    <p>Log Out</p>
+                                    </button>
+                            ):(
+                                <button className="auth-button" onClick={auth.signIn}>
+                                    <p>Sign In</p>
+                                </button>
+                            )}
+                        </>
+                    )}
+                </section>
+            </div>
+        </main>
+    )
+}
+export default Auth
